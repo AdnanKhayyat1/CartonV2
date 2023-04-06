e19b1c17eb760e53e090ab735298a125da8ef70d
Adnan Khayyat <adnankhayyat@adnans-air-2.lan>
2023-04-05 22:08:31 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-import disableReactDevTools from "@fvilers/disable-react-devtools";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
